863223719c01798d6fb91ce25e6af8e14fed147c
vincio <airfoil_elector0b@icloud.com>
2021-08-02 21:43:30 +0800

    Initial Commit

---
+++ sandbox-day1/sandbox_day1App.swift
@@ -0,0 +1,17 @@
+//
+//  sandbox_day1App.swift
+//  sandbox-day1
+//
+//  Created by Vincio on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct sandbox_day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
